d7abb1c2ba53e608ef172825623429746b1d855e
eranees <eraneesakbar@gmail.com>
2024-06-29 12:49:42 +0530

    SWagger Setup

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle(`Portfolio API's`)
-    .setDescription('Portfolio API description')
+    .setTitle('API Documentation')
+    .setDescription('API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      url: '/static/swagger.json',
+    },
+  });
 
   await app.listen(3000);
 }

52131ec1a39bbc98dd94fa6bbd3d6f796d7e71dd
eranees <eraneesakbar@gmail.com>
2024-06-29 13:11:15 +0530

    Change swagger description

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('API description')
+    .setTitle(`Portfolio-Server API's`)
+    .setDescription('Portfolio-Server API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document, {
-    customSiteTitle: 'Api Docs',
+    customSiteTitle: 'Portfolio-Server API',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
